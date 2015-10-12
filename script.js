db4a64ea60c250f0ea8cc16c24de61da4d664b8e
xdnewlun <xdnewlun@yahoo.com>
2015-10-12 18:29:33 -0500

    Changed ALOT
    added +10, and +100
    changed color white,
    added upgrade costs!

--- script.js
+++ script.js
@@ -11,13 +11,33 @@ var clickAmount = 1;
 		$("#brownies").text("Brownies: " + brownies);
 	});
 
-	$("#upgrade").click(function(){
+	$("#upgrade1").click(function(){
 		if(brownies >= 10){
 			clickAmount += 1;
 			brownies -= 10;
 			$("#brownies").text("Brownies: " + brownies);
 		}else{
 			alert("Sorry you do not have enought brownies. You need a total of 10. You need " + (10 - brownies) + " more brownies!");
+		}
+	});
+
+	$("#upgrade10").click(function() {
+		if(brownies >= 100){
+			clickAmount += 10;
+			brownies -= 100;
+			$("#brownies").text("Brownies: " + brownies);
+		}else{
+			alert("Sorry you do not have enought brownies. You need a total of 100. You need " + (100 - brownies) + " more brownies!");
+		}
+	});
+
+	$("#upgrade100").click(function(){
+		if(brownies >= 1000){
+			clickAmount += 100;
+			brownies -= 1000;
+			$("#brownies").text("Brownies: " + brownies);
+		}else{
+			alert("Sorry you do not have enought brownies. You need a total of 1000. You need " + (1000 - brownies) + " more brownies!");
 		}
 	});
 });
