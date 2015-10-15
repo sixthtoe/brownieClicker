837744d2dcc95720033d7a451db0bb044bc6d6eb
xdnewlun <xdnewlun@yahoo.com>
2015-10-14 20:45:13 -0500

    ADDED Give Brownies
    -Added Brownie Login
    -Added Brownie Give

--- script.js
+++ script.js
@@ -6,29 +6,49 @@ $(document).ready(function() {
 var brownies = 0;
 var clickAmount = 1;
 var maxUpgrade = false;
+var grandma = 0;
+var bakeSale = 0;
+var girlScout = 0;
+var factory = 0;
+var tries = 0;
+var code = "XS Projects";
+var username = "XS Projects";
 
-	$("#winner").text("To Win you must have 10000000000000 brownies! You need 10000000000000 more to win!");
+	$("#upgrade-max").text("False");
+	$("#factory").text("0");
+	$("#girl-scout").text("0");
+	$("#bake-sale").text("0");
+	$("#grandma").text("0");
+	$("#winner").text("To Win you must have 10000000000000000 brownies! You need 10000000000000000 more to win!");
 	$("#upgraded").text("Per-Click: 1");
 	$("#boughtMax").text("TO USE UPGRADE MAX YOU NEED TO BUY IT");
 	$("#cookie-clicker").click(function(){
 		brownies += clickAmount;
 		$("#brownies").text("Brownies: " + brownies);
 
-		if(brownies >= 10000000000000){
-			$("#winner").text("YOU WIN YOU HAVE " + (brownies - 10000000000000) + " MORE THAN THE GOAL OF 10000000000000!");
+		if(brownies >= 10000000000000000){
+			$("#winner").text("YOU WIN YOU HAVE " + (brownies - 10000000000000000) + " MORE THAN THE GOAL OF 10000000000000000!");
 		}else{
-			$("#winner").text("To Win you must have 10000000000000 brownies! You need " + (10000000000000 - brownies) + " more to win!");
+			$("#winner").text("To Win you must have 10000000000000000 brownies! You need " + (10000000000000000 - brownies) + " more to win!");
 		}
 	});
 
 	$("#reset").click(function() {
+		grandma = 0;
+		bakeSale = 0;
+		girlScout = 0;
+		factory = 0;
 		brownies = 0;
 		clickAmount = 1;
 		maxUpgrade = false;
-		$("#winner").text("To Win you must have 10000000000000 brownies! You need 10000000000000 more to win!");
+		$("#winner").text("To Win you must have 10000000000000000 brownies! You need 10000000000000000 more to win!");
 		$("#brownies").text("Brownies: 0");
 		$("#upgraded").text("Per-Click: 1");
 		$("#boughtMax").text("TO USE UPGRADE MAX YOU NEED TO BUY IT");
+		$("#factory").text("0");
+		$("#girl-scout").text("0");
+		$("#bake-sale").text("0");
+		$("#grandma").text("0");
 	})
 
 	$("#unlockMax").click(function(){
@@ -37,6 +57,7 @@ var maxUpgrade = false;
 			maxUpgrade = true;
 			$("#brownies").text("Brownies: " + brownies);
 			$("#boughtMax").text("MAX UPGRADE HAS BEEN BOUGHT");
+			$("#upgrade-max").text("True");
 		}else{
 			alert("You can not afford to buy the Max Upgrade! You need " + (100000 - brownies) + " more brownies");
 		}
@@ -46,8 +67,10 @@ var maxUpgrade = false;
 		if(brownies >= 10){
 			clickAmount += 1;
 			brownies -= 10;
+			grandma += 1;
 			$("#upgraded").text("Per-Click: " + clickAmount);
 			$("#brownies").text("Brownies: " + brownies);
+			$("#grandma").text(grandma);
 		}else{
 			alert("Sorry you do not have enough brownies. You need a total of 10. You need " + (10 - brownies) + " more brownies!");
 		}
@@ -57,8 +80,10 @@ var maxUpgrade = false;
 		if(brownies >= 100){
 			clickAmount += 10;
 			brownies -= 100;
+			bakeSale += 1;
 			$("#upgraded").text("Per-Click: " + clickAmount);
 			$("#brownies").text("Brownies: " + brownies);
+			$("#bake-sale").text(bakeSale);
 		}else{
 			alert("Sorry you do not have enough brownies. You need a total of 100. You need " + (100 - brownies) + " more brownies!");
 		}
@@ -66,9 +91,11 @@ var maxUpgrade = false;
 
 	$("#upgrade100").click(function(){
 		if(brownies >= 1000){
+			girlScout += 1;
 			clickAmount += 100;
 			brownies -= 1000;
 			$("#upgraded").text("Per-Click: " + clickAmount);
+			$("#girl-scout").text(girlScout);
 			$("#brownies").text("Brownies: " + brownies);
 		}else{
 			alert("Sorry you do not have enough brownies. You need a total of 1000. You need " + (1000 - brownies) + " more brownies!");
@@ -77,10 +104,12 @@ var maxUpgrade = false;
 
 	$("#upgrade1000").click(function() {
 		if(brownies >= 10000){
+			factory += 1;
 			clickAmount += 1000;
 			brownies -= 10000;
 			$("#upgraded").text("Per-Click: " + clickAmount);
 			$("#brownies").text("Brownies: " + brownies);
+			$("#factory").text(factory);
 		}else{
 			alert("Sorry you do not have enough brownies. You need a total of 10000. You need " + (10000 - brownies) + " more brownies!");
 		}
@@ -102,15 +131,63 @@ var maxUpgrade = false;
 		}else{
 			alert("You need to unlock Upgrade Max first. You need " + (100000 - brownies) + " more brownies to unlock!");
 		}	
-	})
+	});
 
-
-	//$("#brownie-set").change(function(){
-	//	brownies = $("brownie-set").val();
-	//	$("#brownies").text("Brownies: " + brownies);
-	//});
-
-	
-
+	$("#brownieSet").click(function(){
+		var uName = prompt("Enter Admin UserName");
+		if(uName === username){
+			var uPassword = prompt("Enter Admin Password");
+			if(uPassword === code){
+				var brownieSetter = prompt("How many brownies do you need?");
+				brownies = brownieSetter;
+				$("#brownies").text("Brownies: " + brownies);	
+				tries = 0;
+			}else{
+				alert("Password Incorrect!");
+				tries ++;
+				alert("You have " + (5 - tries) + " tries remaning before your brownies are deleted!");
+				if(tries >= 5){
+				grandma = 0;
+				bakeSale = 0;
+				girlScout = 0;
+				factory = 0;
+				brownies = 0;
+				clickAmount = 1;
+				maxUpgrade = false;
+				$("#winner").text("To Win you must have 10000000000000000 brownies! You need 10000000000000000 more to win!");
+				$("#brownies").text("Brownies: 0");
+				$("#upgraded").text("Per-Click: 1");
+				$("#boughtMax").text("TO USE UPGRADE MAX YOU NEED TO BUY IT");
+				$("#factory").text("0");
+				$("#girl-scout").text("0");
+				$("#bake-sale").text("0");
+				$("#grandma").text("0");
+				alert("All Atempts Failed, All info deleted!");
+				}
+			}
+		}else{
+			alert("Username Incorrect");
+			tries ++;
+			alert("You have " + (5 - tries) + " tries remaning before your brownies are deleted!");
+			if(tries >= 5){
+				grandma = 0;
+				bakeSale = 0;
+				girlScout = 0;
+				factory = 0;
+				brownies = 0;
+				clickAmount = 1;
+				maxUpgrade = false;
+				$("#winner").text("To Win you must have 10000000000000000 brownies! You need 10000000000000000 more to win!");
+				$("#brownies").text("Brownies: 0");
+				$("#upgraded").text("Per-Click: 1");
+				$("#boughtMax").text("TO USE UPGRADE MAX YOU NEED TO BUY IT");
+				$("#factory").text("0");
+				$("#girl-scout").text("0");
+				$("#bake-sale").text("0");
+				$("#grandma").text("0");
+				alert("All Atempts Failed, All info deleted!");
+			}
+		}
+	});
 });
 
